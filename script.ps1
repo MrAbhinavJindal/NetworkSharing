06d5d252e41d57408547deac2da19addccfbab21
MrAbhinavJindal <106862837+MrAbhinavJindal@users.noreply.github.com>
2022-06-04 14:27:40 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,3 +2,12 @@ $Acl = Get-Acl "D:\"
 $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule('Everyone', 'FullControl', 'ContainerInherit,ObjectInherit', 'None', 'Allow')
 $Acl.SetAccessRule($Ar)
 Set-Acl 'D:\' $Acl
+
+Remove-SmbShare -Name 'D' -Force
+New-SmbShare -Path D:\\ -Name 'D' -FullAccess Everyone
+
+Remove-LocalUser 'Visitor'
+$GuestPassword = Read-Host -AsSecureString
+1234
+New-LocalUser 'Visitor' -Password $GuestPassword
+Add-LocalGroupMember -Group 'Guests' -Member 'Visitor'
